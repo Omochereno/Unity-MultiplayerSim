752e70772cb2aed9322d20fb12648e05b36e49a4
Stijn Lukassen <stijn.lukassen@gmail.com>
2019-05-22 19:53:52 +0200

    Add unity project

---
+++ Co-op Artillerie unit/Assets/BlinkingButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingButton : MonoBehaviour
+{
+
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartBlinking();
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (text.color.a.ToString())
+            {
+
+                case "0":
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+            }
+        }
+    }
+
+    private void StartBlinking()
+    {
+        StopCoroutine("Blink");
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopCoroutine("Blink");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
