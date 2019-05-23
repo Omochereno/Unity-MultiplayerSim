9a0b9617b06d629342af9b167df0a12a5037b72a
Stijn Lukassen <stijn.lukassen@gmail.com>
2019-05-23 13:55:02 +0200

    networking

---
+++ Co-op Artillerie unit/Assets/Scripts/Managers/NetworkManagerC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManagerC : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); // connect to master servers (photon)
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Co-op Artillerie unit/Assets/Scripts/Photon/PhotonPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PhotonPlayer : MonoBehaviour
+{
+    private PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), transform.position, Quaternion.identity, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
